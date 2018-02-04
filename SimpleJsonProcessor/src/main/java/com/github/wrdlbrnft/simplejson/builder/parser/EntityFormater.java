bdd04d5241de10769482cace927182985915ba90
Xaver Kapeller <xaver.kapeller@gmail.com>
2018-02-04 04:41:17 +0100

    Fixed some minor formatting bugs and added a basic test.

--- SimpleJsonProcessor/src/main/java/com/github/wrdlbrnft/simplejson/builder/parser/EntityFormater.java
+++ SimpleJsonProcessor/src/main/java/com/github/wrdlbrnft/simplejson/builder/parser/EntityFormater.java
@@ -1,12 +1,14 @@
 package com.github.wrdlbrnft.simplejson.builder.parser;
 
 import com.github.wrdlbrnft.codebuilder.code.Block;
+import com.github.wrdlbrnft.codebuilder.code.BlockWriter;
 import com.github.wrdlbrnft.codebuilder.code.CodeElement;
 import com.github.wrdlbrnft.codebuilder.elements.forloop.item.Foreach;
+import com.github.wrdlbrnft.codebuilder.elements.ifs.If;
 import com.github.wrdlbrnft.codebuilder.elements.values.Values;
 import com.github.wrdlbrnft.codebuilder.executables.Methods;
 import com.github.wrdlbrnft.codebuilder.types.Types;
-import com.github.wrdlbrnft.codebuilder.variables.Field;
+import com.github.wrdlbrnft.codebuilder.util.Operators;
 import com.github.wrdlbrnft.codebuilder.variables.Variable;
 import com.github.wrdlbrnft.codebuilder.variables.Variables;
 import com.github.wrdlbrnft.simplejson.SimpleJsonTypes;
@@ -32,11 +34,36 @@ class EntityFormater {
         final MethodPairInfo info = mappedValue.getMethodPairInfo();
         final CodeElement parser = mParserResolver.getElementParserField(mappedValue);
 
-        block.append(parser).append(".toJsonObject(")
-                .append(varJsonObject).append(", ")
-                .append(Values.of(mappedValue.getFieldName())).append(", ")
-                .append(Methods.call(info.getGetter(), varEntity))
-                .append(");").newLine();
+        if (mappedValue.isOptional()) {
+            final Variable item = Variables.of(Types.of(info.getGetter().getReturnType()), Modifier.FINAL);
+            block.set(item, Methods.call(info.getGetter(), varEntity)).append(";").newLine();
+
+            block.append(new If.Builder()
+                    .add(Operators.operate(item, "==", Values.ofNull()), new BlockWriter() {
+                        @Override
+                        protected void write(Block block) {
+                            block.append(Methods.stub("put").callOnTarget(varJsonObject, Values.of(mappedValue.getFieldName()), Values.ofNull())).append(";");
+                        }
+                    })
+                    .setElse(new BlockWriter() {
+                        @Override
+                        protected void write(Block block) {
+                            block.append(parser).append(".toJsonObject(")
+                                    .append(varJsonObject).append(", ")
+                                    .append(Values.of(mappedValue.getFieldName())).append(", ")
+                                    .append(item)
+                                    .append(");");
+                        }
+                    })
+                    .build());
+            block.newLine();
+        } else {
+            block.append(parser).append(".toJsonObject(")
+                    .append(varJsonObject).append(", ")
+                    .append(Values.of(mappedValue.getFieldName())).append(", ")
+                    .append(Methods.call(info.getGetter(), varEntity))
+                    .append(");").newLine();
+        }
     }
 
     public void formatList(Block block, MappedValue mappedValue, Variable varJsonObject, Variable varEntity) {
@@ -54,18 +81,48 @@ class EntityFormater {
         final Variable varJsonArray = Variables.of(SimpleJsonTypes.JSON_ARRAY, Modifier.FINAL);
         block.set(varJsonArray, SimpleJsonTypes.JSON_ARRAY.newInstance()).append(";").newLine();
 
-        block.append(new Foreach.Builder()
-                .setItemType(Types.of(type))
-                .setCollection(Methods.call(info.getGetter(), varEntity))
-                .setIteration(new Foreach.Iteration() {
-                    @Override
-                    public void onIteration(Block block, Variable item) {
-                        block.append(parser).append(".toJsonArray(").append(varJsonArray).append(", ").append(item).append(");");
-                    }
-                })
-                .build());
-        block.newLine();
-
-        block.append(varJsonObject).append(".put(").append(Values.of(mappedValue.getFieldName())).append(", ").append(varJsonArray).append(");").newLine();
+        final Variable collection = Variables.of(Types.of(info.getGetter().getReturnType()), Modifier.FINAL);
+        block.set(collection, Methods.call(info.getGetter(), varEntity)).append(";").newLine();
+        if(mappedValue.isOptional()) {
+            block.append(new If.Builder()
+                    .add(Operators.operate(collection, "==", Values.ofNull()), new BlockWriter() {
+                        @Override
+                        protected void write(Block block) {
+                            block.append(Methods.stub("put").callOnTarget(varJsonObject, Values.of(mappedValue.getFieldName()), Values.ofNull())).append(";");
+                        }
+                    })
+                    .setElse(new BlockWriter() {
+                        @Override
+                        protected void write(Block block) {
+                            block.append(new Foreach.Builder()
+                                    .setItemType(Types.of(type))
+                                    .setCollection(collection)
+                                    .setIteration(new Foreach.Iteration() {
+                                        @Override
+                                        public void onIteration(Block block, Variable item) {
+                                            block.append(parser).append(".toJsonArray(").append(varJsonArray).append(", ").append(item).append(");");
+                                        }
+                                    })
+                                    .build());
+                            block.newLine();
+                            block.append(varJsonObject).append(".put(").append(Values.of(mappedValue.getFieldName())).append(", ").append(varJsonArray).append(");").newLine();
+                        }
+                    })
+                    .build());
+            block.newLine();
+        } else {
+            block.append(new Foreach.Builder()
+                    .setItemType(Types.of(type))
+                    .setCollection(collection)
+                    .setIteration(new Foreach.Iteration() {
+                        @Override
+                        public void onIteration(Block block, Variable item) {
+                            block.append(parser).append(".toJsonArray(").append(varJsonArray).append(", ").append(item).append(");");
+                        }
+                    })
+                    .build());
+            block.newLine();
+            block.append(varJsonObject).append(".put(").append(Values.of(mappedValue.getFieldName())).append(", ").append(varJsonArray).append(");").newLine();
+        }
     }
 }
